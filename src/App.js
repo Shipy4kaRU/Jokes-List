2cd74a7972c4ae54a50d23502dc091f50793d2f0
Danil Shipy4ka <105577586+Shipy4kaRU@users.noreply.github.com>
2024-09-24 10:55:58 +0400

    finished

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useEffect, useState } from "react";
+
+import JokeList from "./components/JokeList";
+import "./App.css";
 
 function App() {
+  const [jokes, setJokes] = useState([]);
+  const [isLodaing, setIsLoading] = useState(false);
+  const [errors, setErrors] = useState(null);
+
+  const fetchJokesHandler = useCallback(async () => {
+    try {
+      setErrors(null);
+      setIsLoading(true);
+      const response = await fetch(
+        "https://official-joke-api.appspot.com/random_ten"
+      );
+      if (!response.ok) throw new Error("Что-то пошло не так...");
+      const data = await response.json();
+      setJokes(data);
+    } catch (err) {
+      setErrors(err.message);
+    }
+    setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    fetchJokesHandler();
+  }, [fetchJokesHandler]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <section>
+        <button onClick={fetchJokesHandler}>Fetch Jokes</button>
+      </section>
+      <section>
+        {!isLodaing && errors && <p>{errors}</p>}
+        {!isLodaing && <JokeList jokes={jokes} />}
+        {isLodaing && <p>Загрузка шуток...</p>}
+      </section>
+    </React.Fragment>
   );
 }
 
